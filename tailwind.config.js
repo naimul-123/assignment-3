23fd99a68e1c558565e5db99aeee0e7ca3fabd49
Naimul-Islam <naimuliu@gmail.com>
2024-01-23 23:18:57 +0600

    assignment 3 is running

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          'oxford-blue': 'rgba(19,19,24)',
+          
+        },
+      },
+    },
+    plugins: [],
+  }
